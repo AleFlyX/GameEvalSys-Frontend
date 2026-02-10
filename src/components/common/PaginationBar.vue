edb8d77b834bc7a72471b131e489e2520c7e787e
LJR <1449466458@qq.com>
2026-02-10 20:52:55 +0800

    init:初始化分页和搜索通用组件封装

--- src/components/common/PaginationBar.vue
+++ src/components/common/PaginationBar.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="demo-pagination-block">
     <div class="demonstration">Change page size</div>
-    <el-pagination v-model:current-page="currentPage2" v-model:page-size="pageSize2" :page-sizes="[100, 200, 300, 400]"
+    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[10, 20, 30]"
       :size="size" :disabled="disabled" :background="background" layout="sizes, prev, pager, next" :total="1000"
       @size-change="handleSizeChange" @current-change="handleCurrentChange" />
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
+const emitData = defineEmits({
+
+})
+const currentPage = ref(1);
+const pageSize = ref(10);
+const disabled = ref(true);
 </script>
---
+++ src/components/common/SearchInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="width: 50%; display: flex; gap: 10px;">
+    <el-input v-model="search" size="small" placeholder="Type to search" />
+    <el-button>查找</el-button>
+    <el-button>添加</el-button>
+  </div>
+</template>
+
+<script setup>
+</script>
