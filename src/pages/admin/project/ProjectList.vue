7d497ed185d786d89a23243d6fd248ce9d3c9bf5
LJR <1449466458@qq.com>
2026-02-10 20:54:59 +0800

    init:初始化项目和统计管理界面

--- src/pages/admin/project/ProjectList.vue
+++ src/pages/admin/project/ProjectList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-
+    project manage
   </div>
 </template>
 
--- src/pages/admin/statistic/StatisticPannel.vue
+++ src/pages/admin/statistic/StatisticPannel.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="">
+  <div class="statistic-panel">
+    statistic
+    <el-table :data="tableData" style="width: 100%" stripe>
+      <DataTableColums>
 
+      </DataTableColums>
+    </el-table>
   </div>
 </template>
 
 <script setup>
+import DataTableColums from '@/components/common/DataTableColums.vue';
 
+const tableData = [
+  {
+    id: 131231,
+    username: "admin2323",
+    role: "super_admin",
+    name: "超级管理员",
+    isEnabled: true,
+    createTime: "2026-01-27 09:00:00"
+  },
+]
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
