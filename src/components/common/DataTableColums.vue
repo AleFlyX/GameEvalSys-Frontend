b09cea83ffd49a3897b62c1186cbde0554fbc7aa
LJR <1449466458@qq.com>
2026-02-11 20:22:34 +0800

    feat:数据表格支持tag显示

--- src/components/common/DataTableColums.vue
+++ src/components/common/DataTableColums.vue
@@ -3,18 +3,18 @@
     :label="(rule.label || '空标题')" :width="(rule.width || '100')">
     <template #default="scope">
       <div style="display: flex; align-items: center;">
-        <el-icon v-if="rule.icon !== 'none' || rule.icon">
-          <component v-if="rule.icon !== 'none' || rule.icon" :is="rule.icon"></component>
+        <el-icon v-if="hasIcon(rule)">
+          <component v-if="hasIcon(rule)" :is="rule.icon"></component>
         </el-icon>
-        <!-- <el-tag v-if="rule.tagType" :type="rule.tagType">{{ getColVal(scope.row, rule.colDataName) }}</el-tag> -->
-        <!-- <span v-if="!rule.tagType">{{ getColVal(scope.row, rule.colDataName) }}</span> -->
-        <span>{{ getColVal(scope.row, rule.colDataName) }}</span>
+        <el-tag v-if="hasTagCols(rule)" :type="rule.tagTypeMap[getColVal(scope.row, rule.colDataName)]">
+          {{ rule.tagNameMap[getColVal(scope.row, rule.colDataName)] }}
+        </el-tag>
+        <span v-if="!hasTagCols(rule)">{{ getColVal(scope.row, rule.colDataName) }}</span>
       </div>
     </template>
   </el-table-column>
 </template>
 <script setup>
-
 const props = defineProps({
   colRules: {
     type: Array,
@@ -25,11 +25,37 @@ const props = defineProps({
         colDataName: 'id',
         icon: 'none',
         // tagType: 'none'
+        // tagTypeMap: {
+        //   super_admin: 'primary',
+        //   admin: 'primary',
+        //   scorer: 'success',
+        //   normal: 'info'
+        // },
+        // tagNameMap: {
+        //   super_admin: '超级管理员',
+        //   admin: '管理员',
+        //   scorer: '打分用户',
+        //   normal: '普通用户'
+        // },
       }
     ]
   }
 
 })
+const hasIcon = (rule) => {
+  if (rule.icon !== 'none' || rule.icon) {
+    return true;
+  }
+  return false;
+}
+
+const hasTagCols = (rule) => {
+  if (rule.tagTypeMap && rule.tagNameMap) {
+    // console.log('hAS Tags', rule.tagTypeMap)
+    return true;
+  }
+  return false;
+}
 
 const getColVal = (row, colDataName) => {
   if (!colDataName) return 'Empty Data';
@@ -39,5 +65,5 @@ const getColVal = (row, colDataName) => {
 }
 
 
-console.log(props.colRules)
+console.log('COLUMNS RUle:', props.colRules)
 </script>
