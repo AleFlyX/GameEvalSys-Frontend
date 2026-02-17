62486c9c402ce61d94c0be91b0ef3775be086250
LJR <1449466458@qq.com>
2026-02-18 02:43:28 +0800

    perf:优化modal组件三层的props传递行为

--- src/pages/admin/user/components/UserConfirmDelete.vue
+++ src/pages/admin/user/components/UserConfirmDelete.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseConfirmModal v-model:visible="dialogVisible">
+  <BaseConfirmModal v-bind="$attrs" @update:visible="$emit('update:visible', $event)">
     <template #title>
       确认删除?
     </template>
@@ -7,22 +7,26 @@
       用户: {{ props.data.name }} id: {{ props.data.id }}
     </template>
     <template #operations>
-      <button class="confirm-btn" @click="handleConfirm()" :disabled="disableBtn">确认</button>
+      <button class="primary-btn" @click="handleConfirm()" :disabled="disableBtn">确认</button>
       <button class="cancel-btn" @click="handleClose()">取消</button>
     </template>
   </BaseConfirmModal>
 </template>
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import BaseConfirmModal from '@/components/common/modal/BaseConfirmModal.vue';
 import { userApi } from '@/api/user'
 import { ElMessage } from 'element-plus';
+
+defineOptions({
+  inheritAttrs: false
+})
 const props = defineProps({
-  visible: {
-    type: Boolean,
-    require: true,
-    default: false
-  },
+  // visible: {
+  //   type: Boolean,
+  //   require: true,
+  //   default: false
+  // },
   data: {
     type: Object,
     default: () => {
@@ -38,15 +42,15 @@ const emits = defineEmits([
   'update:visible'
 ])
 // 定义本地响应式变量，初始值同步 props.visible
-const dialogVisible = ref(props.visible)
-// 监听 props.visible 变化，同步到本地变量（父组件修改时，子组件跟着变）
-watch(() => props.visible, (newVal) => {
-  dialogVisible.value = newVal
-})
-// 监听本地变量变化，触发 update:visible 通知父组件（子组件修改时，同步给父组件）
-watch(dialogVisible, (newVal) => {
-  emits('update:visible', newVal)
-})
+// const dialogVisible = ref(props.visible)
+// // 监听 props.visible 变化，同步到本地变量（父组件修改时，子组件跟着变）
+// watch(() => props.visible, (newVal) => {
+//   dialogVisible.value = newVal
+// })
+// // 监听本地变量变化，触发 update:visible 通知父组件（子组件修改时，同步给父组件）
+// watch(dialogVisible, (newVal) => {
+//   emits('update:visible', newVal)
+// })
 
 
 // console.log(props.data)
@@ -65,12 +69,14 @@ const handleConfirm = async () => {
     console.log(err)
   }
   finally {
-    dialogVisible.value = false;
+    // dialogVisible.value = false;
+    emits('update:visible', false)
     disableBtn.value = false;
   }
 }
 const handleClose = () => {
   // console.log('UserConfirmDelete Emit CLOSE')
-  dialogVisible.value = false;
+  // dialogVisible.value = false;
+  emits('update:visible', false)
 }
 </script>
