e7362ecb6e164a8285eb978c2e846f3277000fd5
LJR <1449466458@qq.com>
2026-02-22 02:32:50 +0800

    fix:数据逻辑预留

--- src/pages/admin/user/components/UserConfirm.vue
+++ src/pages/admin/user/components/UserConfirm.vue
@@ -4,17 +4,21 @@
       {{ props.title }}
     </template>
     <template #content>
-      用户: {{ props.data.name }} id: {{ props.data.id }}
+      用户: {{ props.data.value.name }} (id: {{ props.data.value.id }})
     </template>
     <template #operations>
-      <button class="primary-btn" @click="handleConfirm()" :disabled="props.disableBtn">确认</button>
+      <button class="primary-btn" @click="handleConfirm()" :disabled="disableBtn">确认</button>
       <button class="cancel-btn" @click="handleClose()">取消</button>
     </template>
   </BaseConfirmModal>
 </template>
 <script setup>
 import BaseConfirmModal from '@/components/common/modal/BaseConfirmModal.vue';
+
+import { ref, watch } from 'vue';
 import { ElMessage } from 'element-plus';
+import { requestHandler } from '@/utils/requestHandler';
+import userApi from '@/api/user';
 
 defineOptions({
   inheritAttrs: false
@@ -31,17 +35,15 @@ const props = defineProps({
   // },
   data: {
     type: Object,
-    default: () => {
-      return {
-        name: 'empty name',
-        id: 'empty id'
-      }
-    }
+    default: () => ({
+      name: 'empty name',
+      id: 'empty id'
+    })
   },
-  disableBtn: {
-    type: Boolean,
-    default: false
-  }
+  // disableBtn: {
+  //   type: Boolean,
+  //   default: false
+  // }
 })
 
 const emits = defineEmits([
@@ -51,8 +53,9 @@ const emits = defineEmits([
 // 定义本地响应式变量，初始值同步 props.visible
 // const dialogVisible = ref(props.visible)
 // // 监听 props.visible 变化，同步到本地变量（父组件修改时，子组件跟着变）
-// watch(() => props.visible, (newVal) => {
-//   dialogVisible.value = newVal
+
+// watch(() => props.data.value, (newVal) => {
+
 // })
 // // 监听本地变量变化，触发 update:visible 通知父组件（子组件修改时，同步给父组件）
 // watch(dialogVisible, (newVal) => {
@@ -60,12 +63,26 @@ const emits = defineEmits([
 // })
 
 /**
- * 新思路：确认之后执行传入的callback函数
+ *  确认之后执行API
  */
+const disableBtn = ref(false);
 const handleConfirm = async () => {
-  // ElMessage.info(`正在删除 ${props.data.name}`);
-  const tmpId = props.data.id;
-  emits('confirm', tmpId)
+  ElMessage.info(`正在删除 ${props.data.name}`);
+  // const userId = props.data.id;
+  disableBtn.value = true;
+  setTimeout(() => {
+    disableBtn.value = false;
+  }, 1000)
+  // try {
+  //   const { success, data } = await requestHandler(userApi.deactiveUser, [userId])
+
+  // } catch (err) {
+
+  // } finally {
+  //   disableBtn.value = true;
+  // }
+  //deactive user API
+  // emits('confirm', userId)
 }
 const handleClose = () => {
   emits('update:visible', false)
