ee0246507bd83942e1dedbf4ff6679fde037681e
LJR <1449466458@qq.com>
2026-02-27 11:44:04 +0800

    fix:业务组件解耦

--- src/pages/normal/scoring/components/ProjectDetails.vue
+++ src/pages/normal/scoring/components/ProjectDetails.vue
@@ -4,7 +4,7 @@
     <div class="project-detail">
       <div class="detail-item">
         <label>项目名称:</label>
-        <span>{{ selectedProject?.name }}</span>
+        <span>{{ selectedProject.name }}</span>
       </div>
       <div class="detail-item">
         <label>项目描述:</label>
@@ -20,14 +20,27 @@
       </div>
       <div class="detail-item">
         <label>项目状态:</label>
-        <el-tag :type="getStatusTagType(selectedProject?.status)">
+        <!-- <el-tag :type="getStatusTagType(selectedProject?.status)">
           {{ formatStatus(selectedProject?.status) }}
-        </el-tag>
+        </el-tag> -->
       </div>
     </div>
   </BaseModal>
 </template>
 <script setup>
+import { ref, watch } from 'vue';
 import BaseModal from '@/components/common/modal/BaseModal.vue';
+const props = defineProps({
+  selectedProject: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+// watch(props.selectedProject,
+//   (newVal) => {
+//     console.log(newVal)
+//   }
+// )
 
 </script>
