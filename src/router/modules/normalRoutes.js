ef078df391fe58db8c436cb987254f299856f7f4
LJR <1449466458@qq.com>
2026-02-27 11:20:26 +0800

    fix:添加项目打分路径

--- src/router/modules/normalRoutes.js
+++ src/router/modules/normalRoutes.js
@@ -1,10 +1,16 @@
 // normal页面（打分用户/普通用户，管理员也可访问）
 const Home = () => import("@/pages/normal/home/HomePage.vue");
 const ScoringList = () => import("@/pages/normal/scoring/ScoringList.vue");
-// const GroupScoring = () => import("@/pages/normal/scoring/GroupScoring.vue");
-
+const ProjectScoring = () => import("@/pages/normal/scoring/ProjectScoring.vue");
+const loginTest = () => import("@/pages/public/login/LoginTest.vue")
 export const norm = [
   // normal子路由（打分用户/管理员均可访问）
+  {
+    path: "loginTest",
+    name: "loginTest",
+    component: loginTest,
+    meta: { title: "登陆测试", roles: ["super_admin", "admin", "scorer", "normal"] }, // 可访问角色
+  },
   {
     path: "home",
     name: "home",
@@ -17,10 +23,10 @@ export const norm = [
     component: ScoringList,
     meta: { title: "打分项目列表", roles: ["super_admin", "admin", "scorer"] },
   },
-  // {
-  //   path: "scoring/group/:projectId", //可通过route.params.projectId读取;
-  //   name: "groupScoring",
-  //   component: GroupScoring,
-  //   meta: { title: "小组打分", roles: ["super_admin", "admin", "scorer"] },
-  // },
+  {
+    path: "scoring/:projectId", //可通过route.params.projectId读取;
+    name: "projectScoring",
+    component: ProjectScoring,
+    meta: { title: "项目打分", roles: ["super_admin", "admin", "scorer"], hidden: true },
+  },
 ];
