6c7cc7405379477f326909e226b24c4f241c0899
LJR <1449466458@qq.com>
2026-02-27 21:28:50 +0800

    fix:打分任务列表渲染规则

---
+++ src/pages/normal/scoring/utils/projectListColRules.js
@@ -0,0 +1,38 @@
+//数据表格列定义
+export const columnsRules = [
+  {
+    label: '项目名称',
+    width: '200',
+    colDataName: 'name',
+  },
+  {
+    label: '项目状态',
+    width: '120',
+    colDataName: 'status',
+    tagTypeMap: {
+      not_started: 'info',
+      ongoing: 'success',
+      ended: 'danger'
+    },
+    tagNameMap: {
+      not_started: '未开始',
+      ongoing: '进行中',
+      ended: '已截止'
+    },
+  },
+  {
+    label: '开始日期',
+    width: '150',
+    colDataName: 'startDate',
+  },
+  {
+    label: '结束日期',
+    width: '150',
+    colDataName: 'endDate',
+  },
+  {
+    label: '完成度',
+    width: '100',
+    colDataName: 'completionRate',
+  }
+];
