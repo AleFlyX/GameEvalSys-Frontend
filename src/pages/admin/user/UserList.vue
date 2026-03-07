6343ddcbfdeff365022dae18b90313d55ae2a413
LJR <1449466458@qq.com>
2026-03-07 23:59:22 +0800

    fix:修改业务组件所需数据

--- src/pages/admin/user/UserList.vue
+++ src/pages/admin/user/UserList.vue
@@ -225,6 +225,7 @@ const getUserDataList = async (pageParams = { page: 1, size: 10 }) => {
   try {
     const response = await userApi.getUserList(pageParams);
     tableData.value = response.data.list;
+    totalData.value = response.data.total;
     ElMessage.success('用户数据获取成功')
     return Promise.resolve();
   } catch (error) {
