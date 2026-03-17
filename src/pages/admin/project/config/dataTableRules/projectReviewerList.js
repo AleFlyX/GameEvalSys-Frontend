d1c00aba215143bb41a9581c344fb67fe7e5643e
AleFlyX <1449466458@qq.com>
2026-03-17 14:26:14 +0800

    refactor:数据表约束规则和表单验证规则

---
+++ src/pages/admin/project/config/dataTableRules/projectReviewerList.js
@@ -0,0 +1,21 @@
+export const REVIEWER_LIST_RULES = [
+  {
+    label: '用户ID',
+    width: 'auto',
+    colDataName: 'id',
+    icon: 'UserFilled'
+  },
+  {
+    label: '用户账号',
+    colDataName: 'username',
+
+  },
+  {
+    label: '昵称',
+    colDataName: 'name',
+  },
+  {
+    label: '加入时间',
+    colDataName: 'createTime',
+  },
+]
