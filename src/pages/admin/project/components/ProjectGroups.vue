d2215f5b77a7cf205d733abc2135bd44f0b42d3d
AleFlyX <1449466458@qq.com>
2026-03-18 19:19:57 +0800

    fix:修复用户在删除小组的操作后未保存操作的数据

--- src/pages/admin/project/components/ProjectGroups.vue
+++ src/pages/admin/project/components/ProjectGroups.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tab-content">
     <div class="groups-tips">
+      edited: {{ editedAtLocal }}, cache: {{ goroupIdsCache }}
       <el-alert title="提示" type="info" description="本表格显示该项目关联的所有小组。可以编辑小组信息或删除不再需要的小组。" :closable="false" />
     </div>
     <el-table :data="groupsList" stripe style="margin-top: 20px; width: 100%">
@@ -32,23 +33,35 @@ const props = defineProps({
   projectId: {
     type: [String, Number],
     default: ''
+  },
+  goroupIdsCache: {
+    type: Array,
+    default: () => []
+  },
+  editedAtLocal: {  // 如果已经在本地编辑过一次了，说明已经初始化一次数据了，无需再向后端获取数据
+    type: Boolean,
+    default: false
   }
 })
 const emits = defineEmits([
   'successNotice',//通知父组件显示成功响应
   'errorNotice', //通知父组件显示报错
-  // 'initGroupIds', //初始化表单数据
+  'edited', //已经在本地被编辑过了
   'update:groupIds'//更新小组id的表单数据
 ])
 // 获取项目关联的小组列表
 const groupsList = ref([])
 const fetchProjectGroups = async () => {
+  if (props.editedAtLocal) { // 如果已经在本地编辑过一次了，说明已经初始化一次数据了，无需再向后端获取数据
+    groupsList.value = props.goroupIdsCache;
+    return;
+  };
   try {
     if (props.projectId) {
       const response = await projectGroupApi.getProjectGroups(props.projectId);
       console.log('获取项目关联的小组列表id :', props.projectId, response.data)
       groupsList.value = response.data || [];
-      // emits('initGroupIds', response.data.map(item => item.id))
+      // emits('inited', true)
     }
   } catch (err) {
     emits('errorNotice', err);//通知父组件统一报错
@@ -70,6 +83,7 @@ const handleDeleteGroup = (row) => {
       // console.log('DELETING GROUP', filteredGroupIds)
       emits('update:groupIds', filteredGroupIds.map(item => item.id))
       emits('successNotice', '删除 ' + row.name + ' 成功')
+      emits('edited', { edited: true, cache: filteredGroupIds })
     })
     .catch(() => {
       // 用户取消
