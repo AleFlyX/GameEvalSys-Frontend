1d9505219bde581a13ebf3d923fff1a1fa80735d
AleFlyX <1449466458@qq.com>
2026-03-30 21:19:57 +0800

    init:初始化概览数据pinia

---
+++ src/stores/modules/overViewData.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import { ref } from 'vue';
+
+/**
+ * TODO: 所有的管理面板的统计数据都临时存储
+ */
+export const useOverViewData = defineStore("overViewData", () => {
+  const dataSets = new Set();
+
+
+  return {
+
+  }
+})
