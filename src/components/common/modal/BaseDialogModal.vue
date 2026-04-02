ba93af78f262c1a63a56e02ca3a31c6cd808a5bd
AleFlyX <1449466458@qq.com>
2026-04-03 00:47:02 +0800

    fix:代码整理以通过eslint

--- src/components/common/modal/BaseDialogModal.vue
+++ src/components/common/modal/BaseDialogModal.vue
@@ -36,58 +36,13 @@ defineOptions({
 
 defineProps([]);
 
-const emits = defineEmits([
+defineEmits([
   'update:visible' // 用于双向绑定，通知父组件更新显隐状态
 ])
 
 </script>
 
 <style scoped>
-.modal-mask {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-  transition: opacity 0.3s ease;
-}
-
-.base-form {
-  width: 400px;
-  /* min-height: 200px; */
-  padding: 15px 20px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  background: white;
-  border-radius: 15px;
-  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-}
-
-.default-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 50px;
-  height: 50px;
-  padding: 10px;
-  font-size: 20px;
-  border: none;
-  outline: none;
-  background: transparent;
-  transition: all 0.2s ease-in-out;
-  cursor: pointer;
-}
-
-.default-close:hover {
-  color: #409eff;
-}
-
 .body {
   padding: 5px;
   max-height: 60vh;
