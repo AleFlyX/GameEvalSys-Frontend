ce299214521f67a36c88655ed39c27aee8d28042
AleFlyX <1449466458@qq.com>
2026-04-07 21:50:09 +0800

    fix:优化页面布局

--- src/layouts/PagePanel.vue
+++ src/layouts/PagePanel.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="admin-panel">
     <!-- 避免各个管理面板组件的css部分冗余 -->
-    <div style="display: flex; width: 100%; ">
+    <header>
       <slot name="header">
         <!-- 数据预览小卡片区域 -->
       </slot>
-    </div>
+    </header>
 
     <slot>
       <!-- 其他小组件区域 -->
     </slot>
 
-    <div class="data-list">
+    <main class="data-list">
       <slot name="main-table">
         <!-- el table组件 -->
       </slot>
-      <div class="footer">
+      <footer>
         <slot name="footer">
           <!-- 分页跳转组件等 -->
         </slot>
-      </div>
-    </div>
+      </footer>
+    </main>
 
   </div>
   <slot name="modals">
@@ -38,10 +38,22 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 15px
+  gap: 15px;
 }
 
-.data-list {
+header {
+  margin: 15px 10px;
+  display: flex;
+  width: 99%;
+  gap: 10px;
+}
+
+/* 保证在header里显示的数据卡片的宽度都一样 */
+header>* {
+  flex: 1;
+}
+
+main {
   width: 99%;
   padding: 20px 0;
   display: flex;
@@ -52,7 +64,7 @@
 
 }
 
-.footer {
+footer {
   width: 99%;
   display: flex;
   justify-content: center;
