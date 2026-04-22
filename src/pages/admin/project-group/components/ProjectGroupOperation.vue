736f40a6ee509ec436d3d400d48b42adc3cbf3b6
AleFlyX <1449466458@qq.com>
2026-04-22 20:21:42 +0800

    feat:完成项目小组批量添加功能

--- src/pages/admin/project-group/components/ProjectGroupOperation.vue
+++ src/pages/admin/project-group/components/ProjectGroupOperation.vue
@@ -1,11 +1,20 @@
 <template>
-  <BaseFormModal v-bind="$attrs" @update:visible="emits('update:visible', $event)">
+  <BaseFormModal v-bind="$attrs" @update:visible="emits('update:visible', $event)" width="35%" min-height="58%">
     <template #title>
       {{ editMode ? '编辑项目小组' : '新建项目小组' }}
     </template>
     <template #form>
-      <ProjectGroupForm ref="projectGroupFormRef" :data="editingData" :edit-mode="editMode">
-      </ProjectGroupForm>
+      <el-tabs v-model="tab">
+        <el-tab-pane label="单独增加" name="basic">
+          <ProjectGroupForm ref="projectGroupFormRef" :data="editingData" :edit-mode="editMode">
+          </ProjectGroupForm>
+        </el-tab-pane>
+        <el-tab-pane label="批量增加" name="batch">
+          <ProjectGroupMultiAddForm ref="projectGroupMultiAddFormRef">
+          </ProjectGroupMultiAddForm>
+        </el-tab-pane>
+      </el-tabs>
+
     </template>
     <template #operations>
       <button class="primary-btn" @click="handleSubmit" :disabled="isSubmitting">
@@ -21,11 +30,13 @@
 <script setup>
 import { ref, watch } from 'vue';
 import ProjectGroupForm from './ProjectGroupForm.vue';
+import ProjectGroupMultiAddForm from './ProjectGroupMultiAddForm.vue';
 import BaseFormModal from '@/components/common/modal/BaseFormModal.vue';
 import { projectGroupApi } from '@/api/project-group';
 import { ElMessage } from 'element-plus';
 
 const projectGroupFormRef = ref(null);
+const projectGroupMultiAddFormRef = ref(null);
 const isSubmitting = ref(false);
 
 const props = defineProps({
@@ -45,6 +56,7 @@ const props = defineProps({
 
 const emits = defineEmits(['update:visible', 'refresh']);
 
+const tab = ref('basic');
 // 初始化表单数据
 const editingData = ref({ name: '', description: '', isEnabled: 1 });
 
@@ -58,18 +70,18 @@ watch(() => props.data, (newData) => {
 }, { immediate: true, deep: true });
 
 const handleSubmit = async () => {
-  if (!projectGroupFormRef.value) {
+  if ((tab.value === 'basic' && !projectGroupFormRef.value) || (tab.value === 'batch' && !projectGroupMultiAddFormRef.value)) {
     ElMessage.error('表单加载失败');
     return;
   }
 
   isSubmitting.value = true;
-
   try {
-    const { valid, data } = await projectGroupFormRef.value.validate();
+
+    const { valid, data } = tab.value === 'basic' ? await projectGroupFormRef.value.validate() : await projectGroupMultiAddFormRef.value.validate();
 
     if (!valid) {
-      ElMessage.error('表单验证失败');
+      ElMessage.error(`表单验证失败${data?.error || ''}`);
       return;
     }
     console.log(valid, data)
@@ -79,12 +91,19 @@ const handleSubmit = async () => {
       ElMessage.success('小组编辑成功');
     } else {
       // 新建模式
-      await projectGroupApi.createGroups(data);
-      console.log(data)
-      ElMessage.success('小组创建成功');
+      if (tab.value === 'basic') {
+        const response = await projectGroupApi.createGroups(data);
+        emits('refresh', response.data);
+        console.log(data)
+        ElMessage.success('小组创建成功');
+      }
+      else if (tab.value === 'batch') {
+        const response = await projectGroupApi.batchCreateGroups(data);
+        emits('refresh', response.data?.list || []);
+        console.log(data)
+        ElMessage.success('小组批量创建成功');
+      }
     }
-
-    emits('refresh');
     handleCancel();
   } catch (error) {
     console.error('提交失败:', error);
@@ -92,13 +111,14 @@ const handleSubmit = async () => {
   } finally {
     isSubmitting.value = false;
   }
+
 };
 
 const handleCancel = () => {
   emits('update:visible', false);
 };
 
-defineExpose({
-  projectGroupFormRef
-});
+// defineExpose({
+//   projectGroupFormRef
+// });
 </script>
