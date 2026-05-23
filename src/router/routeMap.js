8c9f2b1ed6eaaeabbe8fc25c72d0007355cab13a
AleFlyX <1449466458@qq.com>
2026-05-23 23:52:26 +0800

    fix:补齐routeMap映射

--- src/router/routeMap.js
+++ src/router/routeMap.js
@@ -10,18 +10,25 @@ export const routeMap = {
   // 扩展映射
   'normal-scoring-list': () => import('@/pages/normal/scoring/list/index.vue'),
   'normal-scoring-groups': () => import('@/pages/normal/scoring/groups/index.vue'),
+  'normal-project-scoring': () => import('@/pages/normal/scoring/groups/index.vue'),
   'admin-project-edit': () => import('@/pages/admin/project/edit/index.vue'),
   'admin-project-group': () => import('@/pages/admin/project-group/index.vue'),
+  'admin-project-statistic': () => import('@/pages/admin/project/statistic/index.vue'),
+  'admin-project-statistic-detail': () => import('@/pages/admin/project/statistic/detail/index.vue'),
   'admin-reviewer-group': () => import('@/pages/admin/reviewer-group/ReviewerGroupList.vue'),
+  'admin-reviewer-group-upsert': () => import('@/pages/admin/reviewer-group/upsert/index.vue'),
+  'admin-scoring-stds': () => import('@/pages/admin/scoring-std/index.vue'),
   'admin-user': () => import('@/pages/admin/user/index.vue'),
   'admin-statistic': () => import('@/pages/admin/statistic/StatisticPannel.vue'),
   'public-login': () => import('@/pages/public/login/LoginPage.vue'),
   'public-notfound': () => import('@/pages/public/404/NotFound.vue'),
+  'super-monitor-online': () => import('@/pages/super-admin/monitor/online/index.vue'),
   'super-monitor-server': () => import('@/pages/super-admin/monitor/server/index.vue'),
 };
 
 export function mapComponent(componentCode) {
-  const loader = componentCode ? routeMap[componentCode] : null;
+  if (!componentCode) return null;
+  const loader = routeMap[componentCode];
   return loader || NotFound;
 }
 
